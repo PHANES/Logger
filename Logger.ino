d7d0a1f94cb74ef5c3ae2a5782d6ab4fce60de67
PHANES <m.schonholzer@gmail.com>
2015-02-20 22:35:00 +0100

    simpel working menu

--- Logger.ino
+++ Logger.ino
@@ -8,7 +8,8 @@ int tempAddress = 1;
 #include <ClickEncoder.h>
 #include <TimerOne.h>
 ClickEncoder *encoder;
-int last, value;
+int last = 1;
+int value = 1;
 void timerIsr() {
   encoder->service();
 }
@@ -47,6 +48,7 @@ byte temp, light;
 
 /*######################################## Setup ########################################*/
 void setup() {
+  Serial.begin(9600);
   // Initialize the rtc object
   rtc.begin();
   
@@ -68,12 +70,6 @@ void setup() {
 
 /*######################################## Main Loop ########################################*/
 void loop() {
-  ClickEncoder::Button b = encoder->getButton();
-  if (b != ClickEncoder::Open) {
-    if (ClickEncoder::Clicked) {
-      selector += 1;
-      if (selector == 5) selector = 1;}
-  }
   menu(selector);
   delay(10);
 }
@@ -88,54 +84,127 @@ void splashScreen() {
   display.print(appVersion, RIGHT, 16);
   display.print("Date:", LEFT, 32);
   display.print(versionDate, RIGHT, 32);
-  delay(4000);
   digitalWrite(displayLight, HIGH);
 }
 
-void confirm(int conf) {
-  digitalWrite(displayLight, LOW);
+void readEncoder() {
+  value += encoder->getValue();
+  
+  if (value != last) {
+    if (value > 4 || value < 1) {
+      if (value < 1) {
+        value = 4;
+      }
+      else {
+        value = 1;
+      }
+    }
+    last = value;
+    selector = value;
+  } 
+}
+
+void confirm(int conf, int from) {
   display.clrScr();
+  digitalWrite(displayLight, LOW);
   display.printNumI(conf, CENTER, 0, 2, '0');
   display.print(" Saved!", CENTER, 8);
   delay(2000);
-  selector = 1;
+//  selector = 3;
+  menu(from);
 }
 
 void screenSaver() {
-  digitalWrite(displayLight, HIGH);
+  readEncoder();
   display.clrScr();
+  digitalWrite(displayLight, HIGH);
   display.print(rtc.getDateStr(FORMAT_LONG, FORMAT_BIGENDIAN, '/'), CENTER, 11);
   display.print(rtc.getTimeStr(), CENTER, 29);
 }
 
 void readLog() {
-  digitalWrite(displayLight, LOW);
   display.clrScr();
+  digitalWrite(displayLight, LOW);
   display.print("Stored data", CENTER, 0);
   display.print("-----------------", CENTER, 8);
   display.print("Light: ", LEFT, 16);
   display.printNumI(light, RIGHT, 16);
   display.print("Temp.: ", LEFT, 24);
   display.printNumI(temp, RIGHT, 24);
-  selector = 1;
-  delay(5000);
+  while (selector == 2)
+  {
+    readEncoder();
+  }
 }
 
 void setLight() {
-  ClickEncoder::Button bu = encoder->getButton();
-  while (bu != ClickEncoder::Open && !ClickEncoder::Clicked) {
-    light += encoder->getValue();
-        if (light >= 25) light = 24;
-        if (light < 0) light = 0;
-    display.clrScr();
-    display.print("Set Timer", CENTER, 0);
-    display.print("---------", CENTER, 8);
-    display.print("Lights on for: ", CENTER, 16);
-    display.printNumI(light, CENTER, 24);
-    display.print("hours", CENTER, 32);
+  display.clrScr();
+  digitalWrite(displayLight, LOW);
+  display.print("Click to", CENTER, 0);
+  display.print("set timer.", CENTER, 8);
+  while (selector == 3) {
+    ClickEncoder::Button b = encoder->getButton();
+    readEncoder();
+    if (b == ClickEncoder::Clicked) {
+      display.clrScr();
+      display.print("Set Timer", CENTER, 0);
+      display.print("---------", CENTER, 8);
+      display.print("Lights on for: ", CENTER, 16);
+      display.print("hours", CENTER, 32);
+      while (true) {
+        ClickEncoder::Button b = encoder->getButton();
+        light += encoder->getValue();
+        if (light > 24 || light < 0) {
+          light = 0;
+        }
+        display.printNumI(light, CENTER, 24, 2, '0');
+        if (b == ClickEncoder::DoubleClicked) {
+          menu(selector);
+          break;
+        }
+        if (b == ClickEncoder::Held) {
+          EEPROM.write(lightAddress, light);
+          confirm(light, selector);
+          break;
+        }
+      }
+    }
   }
-  EEPROM.write(lightAddress, light);
-  confirm(light);
+}
+
+void setTemp() {
+  display.clrScr();
+  digitalWrite(displayLight, LOW);
+  display.print("Click to", CENTER, 0);
+  display.print("set temp.", CENTER, 8);
+  while (selector == 4) {
+    ClickEncoder::Button b = encoder->getButton();
+    readEncoder();
+    if (b == ClickEncoder::Clicked) {
+      display.clrScr();
+      display.print("Set Temp.", CENTER, 0);
+      display.print("---------", CENTER, 8);
+      display.print("Max. temp.: ", CENTER, 16);
+      display.print("celsius", CENTER, 32);
+      while (true) {
+        ClickEncoder::Button b = encoder->getButton();
+        temp += encoder->getValue();
+        if (temp > 30 || temp < 20) {
+          temp = 20;
+        }
+        display.printNumI(temp, CENTER, 24, 2, '0');
+        if (b == ClickEncoder::DoubleClicked) {
+          menu(selector);
+          break;
+        }
+        if (b == ClickEncoder::Held) {
+          EEPROM.write(tempAddress, temp);
+          confirm(temp, selector);
+          break;
+        }
+      }
+    }
+  }
 }
 
 void menu(int switcher) {
@@ -148,6 +217,9 @@ void menu(int switcher) {
       break;
     case 3:
       setLight();
+      break;
+    case 4:
+      setTemp();
       break;
     default: 
       screenSaver();
